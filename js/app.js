0c5a1189433fd8e7a62587c481f2aa4d0f3ec710
Hikmahx <hikmayousuph@gmail.com>
2021-01-10 21:58:34 -0800

    vairables and event listener added to make  http call to shrtcode

--- js/app.js
+++ js/app.js
@@ -1,6 +1,38 @@
+// UI for hamburger menu
 const menu = document.querySelector('.hamburger-menu');
 const nav  = document.querySelector('nav');
 
 menu.addEventListener('click', ()=>{
   nav.classList.toggle('open');
 })
+
+
+
+// init Shrtcode
+const shrtcode = new ShrtCode;
+
+// UI variables for API
+const form = document.getElementById('search-form');
+const input = document.getElementById('input-link');
+const submitBtn  = document.querySelector('.submit');
+
+// Search input event listener
+form.addEventListener('submit', (e)=>{
+  const url = input.value
+  
+  if(input.value !==''){
+    // Make http call
+    shrtcode.postUrl(url)
+    .then(data=>{
+      console.log(data.original_link);
+      console.log(data.full_short_link);
+      // console.log(data)
+    })
+    
+    console.log()
+  } else{
+    console.log('nothing in the input');
+  }
+  e.preventDefault();
+})
+
