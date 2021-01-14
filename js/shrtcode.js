01ffef18fc9accaaa15a5cb73fb2252263967be5
Hikmahx <hikmayousuph@gmail.com>
2021-01-13 22:38:54 -0800

    js modified

--- js/shrtcode.js
+++ js/shrtcode.js
@@ -3,7 +3,7 @@ class ShrtCode{
     const response  = await fetch(`https://api.shrtco.de/v2/shorten?url=${url}`);
     
     const data = await response.json();
-    return data.result;
+    return data;
   }
 }
 
